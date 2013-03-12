abbbc012d42fbc8a39cac7a6b9deb8172788e4d7
Jochen Hagenstroem <jhagenst@adobe.com>
2013-03-11 22:21:05 -0700

    ideas

---
+++ www/ideas.js
@@ -0,0 +1,5 @@
+PGB Brackets plugin work
+* fix login - z-index of pass set to -1, issue goes away when z-index=0
+* Is there a security issue? How are  pass and AdobeID stored.
+* Need to persist login state
+add log out 
